11dd45a98f21905aa00f091e4594b3c28636bc22
marcelops123 <95228603+marcelops123@users.noreply.github.com>
2023-01-10 18:16:17 -0300

    Update api.ts

--- src/util/api.ts
+++ src/util/api.ts
@@ -4,7 +4,7 @@ import useSWR from "swr";
 
 export  function useUser(cidade: string) {
 	console.log(cidade)
-	const { data, error, mutate } = useSWR(`https://api.hgbrasil.com/weather?format=json-cors&key=f2c63668&city_name=${cidade?.length< 3?'Paracatu' : cidade === undefined?'Paracatu':cidade},MG`, async url => {
+	const { data, error, mutate } = useSWR(`https://api.hgbrasil.com/weather?format=json-cors&key=5c2945bc&city_name=${cidade?.length< 3?'Paracatu' : cidade === undefined?'Paracatu':cidade},MG`, async url => {
 
 		const response = await fetch(url);
 		const data = await response.json();
